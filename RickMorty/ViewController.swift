b8143343af1a5347fb40aa49c0d36129c401032b
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-08-22 14:33:23 +0400

    Initial commit

---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Anton on 22.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        self.title = "Title"
+    }
+
+
+}
+
